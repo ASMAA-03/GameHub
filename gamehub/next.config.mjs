b7e1ea3a2908602c4eb355286ef93c8028d2d350
ASMA <asmaaaldabbas5@gmail.com>
2026-08-23 02:55:21 +0300

    Update Next.js configuration for GameHub

--- gamehub/next.config.mjs
+++ gamehub/next.config.mjs
@@ -1,6 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
+  output: 'export',
+  basePath: '/GameHub',
+  assetPrefix: '/GameHub/',
+  trailingSlash: true,
+
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
