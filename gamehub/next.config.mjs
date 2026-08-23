460f61908b8fac227428356c55a132a9fb6db62c
ASMA <asmaaaldabbas5@gmail.com>
2026-08-23 03:02:31 +0300

    Reformat next.config.mjs and add newline
    Fix formatting and add newline at the end of the file.

--- gamehub/next.config.mjs
+++ gamehub/next.config.mjs
@@ -1,1 +1,3 @@
-/** @type {import('next').NextConfig} */ const nextConfig = { /* config options here */ }; export default nextConfig; {
+/** @type {import('next').NextConfig} */
+const nextConfig = { /* config options here */ }; 
+export default nextConfig;
