3db3a209b20bcf09863d128fb611a0e4c3597394
ASMA <asmaaaldabbas5@gmail.com>
2026-08-23 03:01:26 +0300

    Refactor next.config.mjs for simplified configuration

--- gamehub/next.config.mjs
+++ gamehub/next.config.mjs
@@ -1,13 +1,1 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  basePath: '/GameHub',
-  assetPrefix: '/GameHub/',
-  trailingSlash: true,
-
-  images: {
-    unoptimized: true,
-  },
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */ const nextConfig = { /* config options here */ }; export default nextConfig; {
